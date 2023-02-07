446f987f18209b887eee6b2b51a5e0e2d55941cf
jesus09062015 <113504346+jesus09062015@users.noreply.github.com>
2023-02-08 00:14:06 +0900

    Add files via upload

---
+++ Prototype/aoki/draw_function.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+from typing import List, Tuple
+from functions import get_draw_info, create_connected
+from settings import SCALE_UP
+
+
+def draw_line(image: np.ndarray, pt1: np.ndarray, pt2: np.ndarray) -> np.ndarray:
+    """2つの点を線で結ぶ
+
+    Args:
+        image (np.ndarray): 点を描画する画像
+        pt1 (np.ndarray): 1つ目の点
+        pt2 (np.ndarray): 2つ目の点
+
+    Returns:
+        np.ndarray: 描画後の画像
+    """    
+    red: Tuple[int, int, int] = (0, 0, 255)
+    pt1_coordinate, pt2_coordinate = get_draw_info(pt1, pt2)
+    cv2.line(image, pt1_coordinate, pt2_coordinate, red, thickness=3)
+    return image
+
+
+#骨格を表示する
+def draw_landmarks(image: np.ndarray, landmarks: List) -> np.ndarray:
+
+    annotated_image = image.copy()
+
+    for people_num in range(len(landmarks)):
+        connected_keypoints = create_connected(landmarks, index=people_num)
+        for (pt1, pt2) in connected_keypoints:
+            if((0 in pt1) or (0 in pt2)): continue # 座標をうまく取得できなかったとき
+
+            annotated_image = draw_line(annotated_image, pt1, pt2)
+    
+    return annotated_image
+
+
+#矩形を表示する
+def draw_rectangle(image: np.ndarray, landmarks: List) -> np.ndarray:
+    annotated_image = image.copy()
+    """
+    landmarks[0] 検出した一人目の
+    landmarks[0].data[0] keypoint番号0(鼻)の
+    landmarks[0].data[0][0] x座標
+    landmarks[0].data[0][1] y座標
+    landmarks[0].data[0][2] 正確さ的なやつ
+    """
+    # ランドマークとして検出されている点を囲む矩形を描画する
+    for person_id in range(len(landmarks)):
+        body_rectangle: List[float] = landmarks[person_id].json_data()["bbox"]
+        base_x, base_y, width, height = body_rectangle
+
+        x1 = int(base_x)
+        y1 = int(base_y - 10)
+        x2 = int(base_x+width)
+        y2 = int(base_x+height)
+        # 元の解像度での座標に戻して表示
+        points = get_draw_info([x1,y1], [x2, y2])
+        cv2.rectangle(annotated_image, points[0], points[1], (255, 255, 255))
+    
+    return annotated_image
+
+
+def draw_id(image: np.ndarray, landmarks: List, image_width: int) -> np.ndarray:
+    annotated_image = image.copy()
+
+    for person_id in range(len(landmarks)):
+        body_rectangle: List[float] = landmarks[person_id].json_data()["bbox"]
+        base_x, base_y, area_width, area_height = body_rectangle
+
+        x1 = int(image_width - ((base_x + area_width) * SCALE_UP))
+        y1 = int((base_y - 20) * SCALE_UP)
+
+        # 文字を重畳
+        id_color = (255,255,255)
+        id_txt = "id: " + str(person_id)
+        cv2.putText(annotated_image,id_txt,(x1, y1),cv2.FONT_HERSHEY_SIMPLEX,1.0,id_color,2,cv2.LINE_4)
+    
+    return annotated_image
+
---
+++ Prototype/aoki/test.py
@@ -0,0 +1,78 @@
+import sys
+import cv2
+import numpy as np
+import openpifpaf
+from PIL import Image
+from typing import List, Tuple
+from functions import create_connected
+from draw_function import draw_line,draw_landmarks, draw_rectangle, draw_id
+from settings import SCALE_UP
+
+
+# PCに繋がっているUSBカメラから撮る場合はこれ
+capture = cv2.VideoCapture(0)
+
+if not capture.isOpened(): # 正常に動画読み込めなかったとき
+    print( "Error opening capture device")
+    capture.release() # カメラデバイス閉じる
+    cv2.destroyAllWindows() # 開いているすべてのウィンドウ閉じる
+    exit()
+
+if capture.isOpened(): # 正常に読みこめたとき
+    print( "Device captured correctly",capture)
+
+predictor = openpifpaf.Predictor(checkpoint = "shufflenetv2k16")
+
+
+while capture.isOpened():
+    """
+    success：画像の取得が成功したか
+    frame：RGBの値を持っている3次元の配列データ ex) サイズ (480, 640, 3) 高さ、幅、色チャネル
+    """
+    read_video: Tuple[bool, np.ndarray] = capture.read()
+    success, frame = read_video
+    # print("frame1 =",frame)
+
+    if not success :
+        print( "frame is None" )
+        break
+    
+    
+    resize_frame: np.ndarray = cv2.resize(frame, dsize=None, fx=(1.0 / SCALE_UP), fy=(1.0 / SCALE_UP))
+    predictions, gt_anns, meta = predictor.numpy_image(resize_frame)
+    """
+    predictions：関節座標
+    インデックス：関節座標点
+    """
+    if len(predictions) == 0: continue
+
+    """
+    predictionの中身の確認
+    """
+    print(predictions)
+
+    #骨格を表示
+    annotated_image: np.ndarray = draw_landmarks(frame, predictions)
+    #predictions[0].data[0] : (x,y,c)
+
+    #外接矩形を表示
+    #annotated_image: np.ndarray = draw_rectangle(frame, predictions)
+
+    height = frame.shape[0]
+    width = frame.shape[1]
+    annotated_image = cv2.flip(annotated_image, 1)
+
+    #idの描画
+    annotated_image = draw_id(annotated_image, predictions, width)
+
+    bigger_frame = cv2.resize(annotated_image, (int(width) * 2, int(height) * 2))
+    cv2.imshow('Camera 1',bigger_frame)
+    #cv2.moveWindow("Camera 1", 200,40)
+
+    # ESCキーを押すと終了
+    if cv2.waitKey(100) == 0x1b:
+        print('ESC pressed. Exiting ...')
+        break
+
+capture.release()
+cv2.destroyAllWindows()
