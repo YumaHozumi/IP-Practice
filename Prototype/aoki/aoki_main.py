52e27835dd9027d86ad614466e71cbbce3d1c00c
jesus09062015 <113504346+jesus09062015@users.noreply.github.com>
2023-02-08 20:21:03 +0900

    main文の修正（平川くんのコードをコピー）

--- Prototype/aoki/aoki_main.py
+++ Prototype/aoki/aoki_main.py
@@ -4,39 +4,11 @@ import numpy as np
 import openpifpaf
 from PIL import Image
 from typing import List, Tuple
-from functions import draw_line, create_connected, calculate_cos, created_three_connected
-from settings import SCALE_UP
+from functions import create_connected
+from vector_functions import correct_vectors
+from draw_function import draw_line, draw_landmarks,draw_vectors, draw_rectangle, draw_id
+from settings import SCALE_UP, weight
 
-def draw_landmarks(image: np.ndarray, landmarks: List) -> np.ndarray:
-
-    annotated_image = image.copy()
-    # ランドマークとして検出されている点を囲む矩形を描画する
-    # body_rectangle: List[float] = landmarks[0].json_data()["bbox"]
-    # base_x, base_y, width, height = body_rectangle
-
-    # x1 = int(base_x)
-    # y1 = int(base_y - 10)
-    # x2 = int(base_x+width)
-    # y2 = int(base_x+height)
-    # # 解像度1/4にしたので、4倍して位置を調整
-    # cv2.rectangle(annotated_image, (x1*SCALE,y1*SCALE), (x2*SCALE, y2*SCALE), (255, 255, 255))
-
-    connected_keypoints = create_connected(landmarks, index=0)
-    for (pt1, pt2) in connected_keypoints:
-        if((0 in pt1) or (0 in pt2)): continue # 座標をうまく取得できなかったとき
-        annotated_image = draw_line(annotated_image, pt1, pt2)
-    
-    return annotated_image
-
-def calc(landmarks: np.ndarray, index: int):
-    connected: List[Tuple[np.ndarray, np.ndarray, np.ndarray]] = created_three_connected(landmarks, index)
-    index = 0
-    for (pt1, pt2, pt3) in connected:
-        if((0 in pt1) or (0 in pt2) or (0 in pt3)): continue
-        print("index: ", end="")
-        print(index)
-        index += 1
-        print(calculate_cos(pt1, pt2, pt3))
 
 # PCに繋がっているUSBカメラから撮る場合はこれ
 capture = cv2.VideoCapture(0)
@@ -74,16 +46,34 @@ while capture.isOpened():
     インデックス：関節座標点
     """
     if len(predictions) == 0: continue
-    annotated_image: np.ndarray = draw_landmarks(frame, predictions)
+
+    #骨格を表示
+    #annotated_image: np.ndarray = draw_landmarks(frame, predictions)
     #predictions[0].data[0] : (x,y,c)
+
+    #骨格(ベクトル)を表示
+    annotated_image = frame.copy()
+
+    for person_id in range(len(predictions)):
+        vectors = correct_vectors(predictions, person_id)
+        annotated_image = draw_vectors(annotated_image, vectors)
+        person_vectors: np.ndarray = np.asarray(vectors)
+
+
+    #外接矩形を表示
+    #annotated_image: np.ndarray = draw_rectangle(frame, predictions)
 
     height = frame.shape[0]
     width = frame.shape[1]
     annotated_image = cv2.flip(annotated_image, 1)
+
+    #idの描画
+    annotated_image = draw_id(annotated_image, predictions, width)
+
     bigger_frame = cv2.resize(annotated_image, (int(width) * 2, int(height) * 2))
     cv2.imshow('Camera 1',bigger_frame)
     #cv2.moveWindow("Camera 1", 200,40)
-    calc(predictions, 0)
+
     # ESCキーを押すと終了
     if cv2.waitKey(100) == 0x1b:
         print('ESC pressed. Exiting ...')
