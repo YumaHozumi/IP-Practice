d6204f8f4c15b69695b03142dfabb72a35181ae8
jesus09062015 <aoki2022@habe-lab.org>
2023-02-24 21:11:56 +0900

    reversalコードの修正

--- Prototype/aoki/reversal.py
+++ Prototype/aoki/reversal.py
@@ -1,14 +1,14 @@
 from PIL import Image
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import statistics
-import math
 
 if __name__ == "__main__":
     img = np.asarray(Image.open('MAX76_gjmax20140531_TP_V.jpg'), dtype=np.uint8)
-    img = img[:, ::-1, :]
-    plt.imshow(img)
-    plt.xticks(color="None")
-    plt.yticks(color="None")
-    plt.tick_params(length=0)
-    plt.savefig('MAX76_reverse.jpg')
+    img = cv2.flip(img, 1)
+    im_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imwrite("MAX76_reverse.jpg", im_rgb)
+#    plt.imshow(img)
+#    plt.xticks(color="None")
+#    plt.yticks(color="None")
+#    plt.tick_params(length=0)
+#    plt.savefig('MAX76_reverse.jpg')
