8dbc8b496205d5d8c029369b61f2f2f31daf477d
hirakawa <hirakawa2022@habe-lab.org>
2023-03-07 11:32:46 +0900

    一連のプレイヤー登録作業を行う関数(register)を追加

--- Prototype/hirakawa/synchronize_game/area_settings.py
+++ Prototype/hirakawa/synchronize_game/area_settings.py
@@ -2,7 +2,7 @@ import numpy as np
 
 #登録領域の設定(macでは画像サイズが1280×720)
 X_LIMIT_START = 0
-Y_LIMIT_START = 200
+Y_LIMIT_START = 0
 X_LIMIT_END = 1280
 Y_LIMIT_END = 720
 
--- Prototype/hirakawa/synchronize_game/game_test.py
+++ Prototype/hirakawa/synchronize_game/game_test.py
@@ -6,9 +6,9 @@ from PIL import Image
 from typing import List, Tuple
 from vector_functions import correct_vectors
 from draw_function import draw_vectors, draw_result
+from regist_functions import register
 from calculation import compare_pose, calc_multiSimilarity
-from area_settings import X_LIMIT_START, Y_LIMIT_START, X_LIMIT_END, Y_LIMIT_END
-from settings import SCALE_UP
+from settings import SCALE_UP, X_LIMIT_START, Y_LIMIT_START, X_LIMIT_END, Y_LIMIT_END
 
 
 # PCに繋がっているUSBカメラから撮る場合はこれ
@@ -25,12 +25,10 @@ if capture.isOpened(): # 正常に読みこめたとき
 
 predictor = openpifpaf.Predictor(checkpoint = "shufflenetv2k16")
 
+print("Start System...")
+
 
 while capture.isOpened():
-    """
-    success：画像の取得が成功したか
-    frame：RGBの値を持っている3次元の配列データ ex) サイズ (480, 640, 3) 高さ、幅、色チャネル
-    """
     read_video: Tuple[bool, np.ndarray] = capture.read()
     success, frame = read_video
     # print("frame1 =",frame)
@@ -40,14 +38,8 @@ while capture.isOpened():
         break
     
     
-        #認識を行う領域を制限
-    limit_frame = frame[Y_LIMIT_START:Y_LIMIT_END, X_LIMIT_START:X_LIMIT_END]
-    resize_frame: np.ndarray = cv2.resize(limit_frame, dsize=None, fx=(1.0 / SCALE_UP), fy=(1.0 / SCALE_UP))
+    resize_frame: np.ndarray = cv2.resize(frame, dsize=None, fx=(1.0 / SCALE_UP), fy=(1.0 / SCALE_UP))
     predictions, gt_anns, meta = predictor.numpy_image(resize_frame)
-    """
-    predictions：関節座標
-    インデックス：関節座標点
-    """
     
     annotated_image = frame.copy()
 
@@ -58,7 +50,6 @@ while capture.isOpened():
         #サイズ確認用(テスト)
         #print('({0}, {1})'.format(height, width))
         
-        annotated_image = cv2.rectangle(annotated_image, (X_LIMIT_START, Y_LIMIT_START), (X_LIMIT_END, Y_LIMIT_END), (0,255,0), thickness=2)
         annotated_image = cv2.flip(annotated_image, 1)
         bigger_frame = cv2.resize(annotated_image, (int(width) * 2, int(height) * 2))
         cv2.imshow('Camera 1',bigger_frame)
@@ -84,38 +75,21 @@ while capture.isOpened():
         annotated_image = draw_vectors(annotated_image, vectors)
         people_vectors[person_id] = np.asarray(vectors)
 
-    #print(people_vectors.shape)
-    
-    #一人で検証する用
-    #print(compare_pose(people_vectors[0], people_vectors[0]))
-    
-
-    #外接矩形を表示
-    #annotated_image: np.ndarray = draw_rectangle(frame, predictions)
-
     height = frame.shape[0]
     width = frame.shape[1]
-    annotated_image = cv2.rectangle(annotated_image, (X_LIMIT_START, Y_LIMIT_START), (X_LIMIT_END, Y_LIMIT_END), (0,255,0), thickness=2)
     annotated_image = cv2.flip(annotated_image, 1)
-
-    #idの描画
-    #annotated_image = draw_id(annotated_image, predictions, width)
-    
-    #similarityの描画
-    if(len(predictions) >= 2):
-        similarity = calc_multiSimilarity(people_vectors) * 100
-        #print(similarity)
-        #similarityの描画
-        annotated_image = draw_result(annotated_image, similarity)
 
     bigger_frame = cv2.resize(annotated_image, (int(width) * 2, int(height) * 2))
     cv2.imshow('Camera 1',bigger_frame)
     #cv2.moveWindow("Camera 1", 200,40)
 
-    # ESCキーを押すと終了
-    if cv2.waitKey(10) == 0x1b:
-        print('ESC pressed. Exiting ...')
+    # Enterキーを押すとプレイヤー登録開始
+    if cv2.waitKey(10) == 0x0d:
+        print('Start register...')
+        face_Imgs = register(capture, predictor)
+        print('End register...')
         break
 
+#print(type(face_Imgs))
 capture.release()
 cv2.destroyAllWindows()
