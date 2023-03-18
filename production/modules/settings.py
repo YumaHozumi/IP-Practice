6ef9c5ee60084d1f82952852f2c905ebd2b0d634
YumaHozumi <cpucmptrpython72@gmail.com>
2023-03-18 19:43:24 +0900

    typeエラーが出てたのでリファクタリング

--- production/modules/settings.py
+++ production/modules/settings.py
@@ -22,9 +22,9 @@ lenient = [1.0, 1.0, (1.0 - 1.0)] #甘め
 default_strict = [0.95, 1.6, (1.0 - 0.95)]
 strict = [0.95, 2.5, (1.0 - 0.95)]
 strictest = [0.95, 4.0, (1.0 - 0.95)] #厳格
-strict_weight: np.ndarray = [strict, strict, strictest, strict, strictest, lenient, default_strict, 
-                strict, default_strict, strict, lenient, lenient, lenient]
-strict_weight = (np.array(strict_weight)).T
+strict_weight: np.ndarray = np.array([strict, strict, strictest, strict, strictest, lenient, default_strict, 
+                strict, default_strict, strict, lenient, lenient, lenient])
+strict_weight = strict_weight.T
 #スケール調整用パラメータ
 scale_weight = strict_weight[0] / np.exp(strict_weight[1])
 bias = strict_weight[2]
@@ -35,9 +35,9 @@ largest = 3.0 #影響度最大
 larger = 1.5
 default_impact = 1.0
 small = 0.5 #影響度最小
-score_weight: np.ndarray = [small, larger, largest, larger, largest, default_impact, 
-                            larger, larger, larger, larger, default_impact, small, small]
-score_weight = np.array(score_weight)
+score_weight: np.ndarray = np.array([small, larger, largest, larger, largest, default_impact, 
+                            larger, larger, larger, larger, default_impact, small, small])
+score_weight = score_weight
 
 #完全一致の場合のスコア
 score_perfect: np.ndarray = score_weight * (scale_weight * np.exp(strict_weight[1]) + bias)
